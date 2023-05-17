78b7e35e742eec3614676415a1d9d11f14d33c1b
adhefirmansyah <firmansyah.adhe23@gmail.com>
2023-05-17 21:21:15 +0700

    first

---
+++ lib/utils/network_manager.dart
@@ -0,0 +1,39 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_application_todolistapp/model/todo_item.dart';
+
+class NetworkManager {
+  late final Dio _dio;
+  final baseUrl = 'https://b5a5-103-111-100-43.ngrok-free.app';
+  NetworkManager(){
+    _dio = Dio();
+  }
+
+  Future<List<ToDoItem>> getTodosIsDone (bool isDone)async{
+    final result = await _dio.get(
+      '$baseUrl/todos?isDone=$isDone',
+    );
+  return (result.data as List).map((e) => ToDoItem.fromMap(e)).toList();
+  }
+
+  Future<ToDoItem> postData(ToDoItem item) async {
+    final result = await _dio.post(
+      '$baseUrl/todos',
+      data: item.toMap(),
+    );
+    return ToDoItem.fromMap(result.data);
+  }
+
+  Future<ToDoItem> updateData (ToDoItem item) async {
+    final result = await _dio.put(
+      '$baseUrl/todos/${item.id}',
+      data : item.toMap(),
+    );
+    return ToDoItem.fromMap(result.data);
+  }
+
+  Future<void> deleteData(ToDoItem item) async {
+    await _dio.delete(
+      '$baseUrl/todos/${item.id}',
+    );
+  }
+}
